9f13f046207d417666e071c9bce5da024d81770a
me <@me>
2021-11-30 08:12:41 -0600

    Initial Commit

---
+++ learning app/learning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_appApp.swift
+//  learning app
+//
+//  Created by AM Scratch - Davin Reynolds on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct learning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
